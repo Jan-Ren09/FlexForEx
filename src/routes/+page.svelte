2d7e3e8180090423dfb637103852fbe66c67c1ea
ren <janren.delasalas@gmail.com>
2025-01-29 14:49:57 +0800

    feat: debugging steps to yml

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script>
 	import CurrencyConverter from '$lib/Converter.svelte';
+
+	if (!import.meta.env.VITE_RAPIDAPI_KEY) {
+		console.warn('VITE_RAPIDAPI_KEY is not set');
+	} else {
+		console.log('API key loaded properly!');
+	}
 </script>
 
 <CurrencyConverter />
